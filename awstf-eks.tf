10889ebc573b0a873ff8975ef17b0407544fa97a
zubiks10 <zzubiks@gmail.com>
2024-05-14 11:39:23 +0100

    newtf

---
+++ awstf-eks.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = ["subnet-020b00fa16744db24", "subnet-0177991908be18866"]
+  }
+}
+
+resource "aws_iam_role" "eks_role" {
+  name = "my-eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
